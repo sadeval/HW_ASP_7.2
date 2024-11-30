9b37cbc3556d39aae4c2346d6a9183cfaec3ced4
sadeval <sadeval2011@gmail.com>
2024-11-30 16:07:20 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using ClothesShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            var product = new Product
+            {
+                Id = id,
+                Name = "Santa Claus",
+                Description = "Праздничная картина деда мороза с подарком и звездами.",
+                Price = 199.99m,
+                ImageUrl = "https://png.pngtree.com/png-vector/20240918/ourlarge/pngtree-festive-santa-claus-painting-with-gift-and-stars-png-image_13696795.png"
+            };
+
+            return View("ProductDetails", product);
+        }
+
+        public IActionResult AddReview(string userName, string reviewText)
+        {
+            Console.WriteLine($"Отзыв от {userName}: {reviewText}");
+
+            return RedirectToAction("Details", new { id = 1 });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HW_ASP_7._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
